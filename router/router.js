4fff7b7646f4d19a4d9b2a5d699edd16fefe1ebe
lemonilemon <87176609+lemonilemon@users.noreply.github.com>
2021-07-16 23:45:02 +0800

    Update router.js

--- router/router.js
+++ router/router.js
@@ -1,46 +1,46 @@
 function route(group) {
 	switch(group) {
 		case 1: 
-			return "https://gather.town/invite?token=mzcydF1N";
+			return "https://gather.town/invite?token=eoQDqV3S";
 			break;
 		case 2:
-			return "https://gather.town/invite?token=Joypvhw7";
+			return "https://gather.town/invite?token=zgBQa3Xz";
 			break;
 		case 3:
-			return "https://gather.town/invite?token=4iUmN3Rr";
+			return "https://gather.town/invite?token=16iCHPUL";
 			break;
 		case 4:
-			return "https://gather.town/invite?token=rmtAWaaQ";
+			return "https://gather.town/invite?token=1JoMRI3D";
 			break;
 		case 5:
-			return "https://gather.town/invite?token=MPbbX7si";
+			return "https://gather.town/invite?token=wA3Foidp";
 			break;
 		case 6:
-			return "https://gather.town/invite?token=PLDXea2G";
+			return "https://gather.town/invite?token=Litch76N";
 			break;
 		case 7:
-			return "https://gather.town/invite?token=sSfZV8yj";
+			return "https://gather.town/invite?token=xxB4MP5F";
 			break;
 		case 8:
-			return "https://gather.town/invite?token=EdY1ZOp5";
+			return "https://gather.town/invite?token=KBlyLagH";
 			break;
 		case 9:
-			return "https://gather.town/invite?token=GSwxDznX";
+			return "https://gather.town/invite?token=meRVs6xW";
 			break;
 		case 10:
-			return "https://gather.town/invite?token=eFkjvNpF";
+			return "https://gather.town/invite?token=cqC3YCXM";
 			break;
 		case 11:
-			return "https://gather.town/invite?token=CsQdfByV";
+			return "https://gather.town/invite?token=VcUtB3wa";
 			break;
 		case 12:
-			return "https://gather.town/invite?token=LU0aPUsS";
+			return "https://gather.town/invite?token=B6IOJ91H";
 			break;
 		case 13:
-			return "https://gather.town/invite?token=yDj4LhVi";
+			return "https://gather.town/invite?token=X3JRz9lX";
 			break;
 		case 14:
-			return "https://gather.town/invite?token=u4BcV4hz";
+			return "https://gather.town/invite?token=3edMmZgp";
 			break;
 		default:
 			return "請輸入有效數據";
